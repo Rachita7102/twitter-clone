0c4091763ca1038cac21104a84d51e19b83dbe45
Sunil-jangid <suniljangid7999@gmail.com>
2026-05-01 13:18:54 +0530

    feature/jenkins: trigger event to jenkins

--- src/main/java/com/example/twitter/TwitterApplication.java
+++ src/main/java/com/example/twitter/TwitterApplication.java
@@ -8,6 +8,7 @@ public class TwitterApplication {
 
 	public static void main(String[] args) {
         System.out.println("Starting Twitter Application...");
+
 		SpringApplication.run(TwitterApplication.class, args);
 	}
 
